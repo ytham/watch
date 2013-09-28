e35d7b3ebadfeb81ac715a2f3a6f64dd69fdeda7
Yu Jiang Tham <yoojbruin@gmail.com>
2013-09-28 14:48:39 -0700

    Added html page

--- watch.js
+++ watch.js
@@ -1,13 +1,30 @@
 var Leap = require('leapjs');
 var Five = require('johnny-five');
-var io = require('socket.io');
-var http = require('http');
+var http = require('http').createServer(handler);
+var io = require('socket.io').listen(http);
 var handwriting = require('./handwriting');
 
 var hands = [];
 var fingers = [];
 
 
+// HTTP server request handler
+function handler (req, res) {
+  fs.readFile(__dirname + '/index.html', function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error 500');
+    }
+
+    res.writeHead(200);
+    res.end(data);
+  });
+}
+
+http.listen(8080);
+
+
+// Leap functions
 Leap.loop(function (frame) {
   if (frame.hands.length > 0) {
     var hand = frame.hands[0];
@@ -17,5 +34,6 @@ Leap.loop(function (frame) {
     }
   }
 });
+
 
 
